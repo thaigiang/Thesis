40f1126051f8a2b227d5422040b6c265314450de
Zero <thaigiang.it@gmail.com>
2017-08-23 11:16:02 +0700

    Test commit

--- app/src/main/java/android/com/thesis/MainActivity.java
+++ app/src/main/java/android/com/thesis/MainActivity.java
@@ -46,6 +46,14 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        testingGit();
+    }
+
+
+    public void testingGit()
+    {
+
     }
 
 
